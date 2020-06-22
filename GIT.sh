0a1f0aa09bb414848ad38e2749cf56ef6d197fc1
Amit Saha <justamitsaha@gmail.com>
2020-06-22 12:05:19 +0530

    Adding GIT commands

--- GIT.sh
+++ GIT.sh
@@ -1,31 +1,37 @@
-#<------------ GIT------------------------->
+<-----------------------GIT------------------------------>
 
-git config --global http.proxy http://tms%5<LanID>:<password>@tfsproxy.tfs.toyota.com:80
-git config --system http.proxy http://tms%5<LanID>:<password>@tfsproxy.tfs.toyota.com:80
 
-git clone https://github.com/justamitsaha/mislaneous.git
-
-git config http.sslVerify false
 
 # Install Git
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
+	sudo apt-get update
+	sudo apt-get upgrade
+	sudo apt-get install git
+<-------------------------------------------------------->
+git # Setting up initial configurations or users
+	git config --global user.email "you@example.com"
+	git config --global user.name "Your Name"
+
+	git config --global http.proxy http://tms%5<LanID>:<password>@tfsproxy.tfs.toyota.com:80
+	git config --system http.proxy http://tms%5<LanID>:<password>@tfsproxy.tfs.toyota.com:80
+
+	git config http.sslVerify false
+
+<-------------------------------------------------------->
+
+# for Cloning a re pository
+	git clone https://github.com/justamitsaha/mislaneous.git
+
+#after making changes
+	it status			# this is show the list of modified changes
+
+# for adding files
+
+	git add 'README.MD'		# or to add all
+	git add .
+	git commit -m "adding details of commit"
+
+<-------------------------------------------------------->
 #setting up new repositary in GIT
-get init
-or
-git clone https://github.com/justamitsaha/mislaneous.git
-git add Link.sh  #to add some files
-git commit -m "sh file pushed"  # add files to local repository
-git push -u origin master
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
 
-#to set your account's default identity.Omit --global to set the identity only in this repository.
 
-#for modification
-git status
-git add 'file name'
-git add .
-git commit -m "git"

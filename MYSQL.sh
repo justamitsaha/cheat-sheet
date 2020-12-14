1165eee61338c6127b5ca2914b01c5a90ae43d16
Amit Saha <justamitsaha@gmail.com>
2020-12-14 22:27:01 +0530

    Something

--- MYSQL.sh
+++ MYSQL.sh
@@ -6,11 +6,19 @@ sudo mysql_secure_installation
 
 #richa123
 
+#uninstall sql server
+sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*;
+sudo apt autoremove
+sudo apt autoclean
+sudo rm -rf /etc/mysql /var/lib/mysql
+
+
 
 #SHOWS A LIST OF USERS 
 SELECT user,authentication_string,plugin,host FROM mysql.user;
 
-#add a password to user
+#Alter user add a password to user will not work if logged in with other user who doesn't have CREATE USER privilege(s)
+#also below command is required to allow root login without sudo as it adds password to user table
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'richa123';
 
 service mysql status
@@ -38,8 +46,7 @@ USE MOVIE_MASTER;
 CREATE TABLE MOVIE_MASTER ( 
 	MOVIE_TD INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,NAME VARCHAR(50) NOT NULL,
 	MOVIE_YEAR VARCHAR(10),
-	DESCRIPTION VARCHAR(2
-		50), 
+	DESCRIPTION VARCHAR(250), 
 	GENRE VARCHAR(50),
 	IMDB_RATING VARCHAR(4),
 	ROTTEN_TOMATOES_RATING VARCHAR(4),
@@ -54,7 +61,7 @@ ALTER TABLE MOVIE_MASTER MODIFY COLUMN MOVIE_NAME VARCHAR(50);
 #TABLE DETAILS
 SHOW COLUMNS FROM MOVIE_MASTER;
 
-INSERT INTO MOVIE_MASTER (MOVIE_NAME, DESCRIPTION, MOVIE_YEAR, GENRE, IMDB_RATING, ROTTEN_TOMATOES_RATING, IMAGE_PATH) VALUES 
+INSERT INTO MOVIE_MASTER (NAME, DESCRIPTION, MOVIE_YEAR, GENRE, IMDB_RATING, ROTTEN_TOMATOES_RATING, IMAGE_PATH) VALUES 
 ('The Shawshank Redemption', 
 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
 '1994',
@@ -63,6 +70,7 @@ INSERT INTO MOVIE_MASTER (MOVIE_NAME, DESCRIPTION, MOVIE_YEAR, GENRE, IMDB_RATIN
 '90',
 'https://cdn.telanganatoday.com/wp-content/uploads/2019/08/The-Shawshank-Redemptiony.jpg');
 
+SELECT * FROM MOVIE_MASTER;
 # create users in data base
 
 CREATE USER <USENAME> @<HOST> IDENTIFIED BY <PASSWPRD>;
@@ -71,13 +79,18 @@ GRANT CREATE, DROP, DELETE, INSERT, SELECT, UPDATE <DB_NAME>TO <USENAME>@<HOST>;
 
 # <HOST> CAN BE localhost, certain IP or * eg 
 
-CREATE USER 'amit' @localhost IDENTIFIED BY 'amit123';
-GRANT CREATE, DROP, DELETE, INSERT, SELECT, UPDATE ON MOVIE_MASTER TO 'amit'@'localhost';
+CREATE USER 'amit'@'192.168.0.100' IDENTIFIED BY 'amit123';
+USE MOVIE_MASTER;
+GRANT CREATE, DROP, DELETE, INSERT, SELECT, UPDATE ON MOVIE_MASTER TO 'amit'@'192.168.0.100';
 FLUSH PRIVILEGES;
 
 # to test
 mysql -u amit -p
 
+CREATE USER 'richa'@'localhost' IDENTIFIED BY 'richa@123';
+USE MOVIE_MASTER;
+GRANT CREATE, DROP, DELETE, INSERT, SELECT, UPDATE ON MOVIE_MASTER TO 'richa'@'localhost';
+FLUSH PRIVILEGES;
 
 
 

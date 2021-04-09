90788508404eba48794f257d2bf47707d7444659
mr-robot <mr-robot@localhost.localdomain>
2021-04-09 16:47:35 +0530

    some changes

--- Fedora.sh
+++ Fedora.sh
@@ -2,3 +2,17 @@
 # 1- Un Install and Install Firefox
 # 2- Clone GIT repo
 # 3- terminal settings
+
+Ubuntu command --- Fedora command
+
+apt-get update --- dnf check-update You don't need to do this as dnf updates its cache automatically before performing transactions
+
+apt-get upgrade --- dnf upgrade
+
+apt-get install --- dnf install
+
+apt-get remove --- dnf remove
+
+apt-get purge --- N/A
+
+apt-cache search --- dnf search 

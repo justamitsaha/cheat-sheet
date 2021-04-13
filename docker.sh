eb2ad52f33b469f3f1519f8b620c3520d1c10815
mr-robot2 <mr-robot2@localhost.localdomain>
2021-04-13 17:28:00 +0530

    Docker info

--- docker.sh
+++ docker.sh
@@ -1,3 +1,11 @@
 #docker.sh
-sudo systemctl start docker
-sudo docker run -d -p 80:80 docker/getting-started
+sudo systemctl start docker											# start docker
+sudo docker run -d -p 9999:9999 docker/getting-started
+sudo docker pull jenkins
+sudo docker images 													# list images
+sudo docker rmi 7a86f8ffcb25   										# remove images
+sudo docker inspect jenkins 										# shows details of an image
+sudo docker ps														# list running containers
+sudo docker top ContainerID 
+sudo docker stop ContainerID 
+sudo docker rm ContainerID 

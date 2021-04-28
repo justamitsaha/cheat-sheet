adf8741e8df2a774236cc5a354a222a0742a0be9
justamitsaha <justamitsaha@gmail.com>
2021-04-28 08:14:05 +0530

    some changes

--- docker.sh
+++ docker.sh
@@ -1,8 +1,8 @@
 #docker.sh
 sudo systemctl start docker											# start docker
 
-sudo docker run nginx 												#runs a container if it locally exists else pulls the image for the 1st time
-docker run --name webapp nginx:1.14-alpine							#name a container
+sudo docker run nginx 												# runs a container if it locally exists else pulls the image for the 1st time
+sudo docker run --name webapp nginx:1.14-alpine							#name a container
 sudo docker stop ContainerID
 sudo docker pull jenkins
 
@@ -11,19 +11,20 @@ sudo docker ps														# list running containers
 sudo docker ps -a 													# list all containers
 
 sudo docker inspect jenkins 										# shows details of an image
-sudo docker exec container_name cat /etc/hosts						#execute a command on container
+sudo docker exec container_name cat /etc/hosts						# execute a command on container
 
 sudo docker rmi imageId     										# remove images
 sudo docker rm ContainerID 											# remove container/ first images 
 
 sudo docker top ContainerID 
-sudo docker stop ContainerID 
 
 
-sudo docker run redis:4:0 											#tag
-sudo docker run -d -p 9999:9999 docker/getting-started				#-p for mapping port -d for detacched mode
-sudo docker run -it ContainerID 									#-i for interactive mode t for promt
-sudo docker run -v /opt/datadir:var/lib/mysql mysql 				#map volume so that sql data is persisted locally
+
+
+sudo docker run redis:4:0 											# runs a specific version called tag
+sudo docker run -it ContainerID 									# i for interactive mode t for promt from application in the container
+sudo docker run -d -p 80:9999 docker/getting-started				#-p for mapping port -d for detacched mode 80 is host port 9999 is docker host port
+sudo docker run -v /opt/datadir:var/lib/mysql mysql 				# map volume so that sql data is persisted locally
 
 sudo docker run -d image 											#run in detached mode
 sudo docker attach containerID 										#attach to container

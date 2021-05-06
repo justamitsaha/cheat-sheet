b3d8ab4310936198086a02389be0f303a06c629e
justamitsaha <justamitsaha@gmail.com>
2021-05-06 09:57:05 +0530

    some change

--- webserver.sh
+++ webserver.sh
@@ -1,18 +1,20 @@
 <---------------nginx------------------->
-dnf -y install nginx
-apt -y install nginx
+sudo dnf -y install nginx
+sudo apt -y install nginx
 
-service nginx status /systemctl status nginx 
-service nginx start /service nginx start
-service nginx stop /service nginx stop
-
-
+systemctl status nginx /service nginx status
+systemctl start nginx /service nginx start
+systemctl stop nginx /service nginx stop
+ps -ef|grep nginx
+sudo dnf remove nginx
 
 
 <---------------apache------------------->
 systemctl status httpd /service httpd status
 systemctl start httpd /service httpd start
 systemctl stop httpd /service httpd stop
+ps -ef|grep httpd
+sudo dnf remove httpd
 
 
 sudo chmod 777 /var/www/html -R
